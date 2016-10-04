2bf4ed7ad702b9cede5cc624d181304f24ae230e
Chandler <use.an.alias@gmail.com>
2016-10-03 20:18:38 -0700

    Update sigmoid tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-from logistic.utils import sigmoid
+from logistic.utils import hypothesis, sigmoid
 
 
-def test_sigmoid():
-    assert sigmoid(-1) == 1 / (1 + np.e)
-    assert sigmoid(0) == 0.5
-    assert sigmoid(1) == 1 / (1 + 1/np.e)
+def test_sigmoid_handles_small_values():
+    assert np.isclose(sigmoid(3), 0.952574127)
+
+
+def test_sigmoid_handles_large_values():
+    assert np.isclose(sigmoid(-100), float('3.72007598e-44'))
+
+
+def test_sigmoid_accepts_ndarray():
+    assert np.isclose(sigmoid(np.array([-1, 1])), 
+                      np.array([0.268941421, 0.731058579])).all()
+
+
+# def test_hypothesis():
+#     assert hypothesis(np.array([]), np.array([])) == np.array([])
